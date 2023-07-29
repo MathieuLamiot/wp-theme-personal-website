d4ef385b322d75451ad106eff3ad5d15682d9800
Mathieu Lamiot <mathieu.lamiot@free.fr>
2023-07-29 22:34:47 +0200

    Modified footer mentionning the theme. Fixed the padding above content.

--- footer.php
+++ footer.php
@@ -67,7 +67,7 @@
                                 echo wp_kses_post(business_insights_get_option('copyright_text'));
                             }
                             ?>
-                            <?php printf(esc_html__('Theme: %1$s by %2$s', 'business-insights'), 'Business Insights', '<a href="http://themeinwp.com/" target = "_blank" rel="designer">Themeinwp </a>');?>
+                            <?php printf(esc_html__('- Theme modified from: %1$s by %2$s', 'business-insights'), 'Business Insights', '<a href="http://themeinwp.com/" target = "_blank" rel="designer">Themeinwp </a>');?>
                         </h4>
                     </div>
                 </div>
